c50e2e5ff7dc65c6b726b922b7b0addd7fce0115
lekshmi5353 <ilekshmi.shaji@gmail.com>
2025-03-15 16:25:18 +0530

    Create qn4.py

---
+++ tutorial_1/qn4.py
@@ -0,0 +1,8 @@
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+if a > b:
+    print(f"{a} is greater than {b}")
+elif a < b:
+    print(f"{b} is greater than {a}")
+else:
+    print("Both numbers are equal")

e148330afb290a4ebec2315f58b2cff640c476e1
lekshmi5353 <ilekshmi.shaji@gmail.com>
2025-03-15 18:35:27 +0530

    Create qn14.py

---
+++ tutorial_2/qn14.py
@@ -0,0 +1,8 @@
+import math
+
+def nCr(n, r):
+    return math.factorial(n) // (math.factorial(r) * math.factorial(n - r))
+
+n = int(input("Enter n: "))
+r = int(input("Enter r: "))
+print(f"nCr: {nCr(n, r)}")

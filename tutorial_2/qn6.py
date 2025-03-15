d962908fabcd96f8c742569fd01e496438a175f0
lekshmi5353 <ilekshmi.shaji@gmail.com>
2025-03-15 18:31:08 +0530

    Create qn6.py

---
+++ tutorial_2/qn6.py
@@ -0,0 +1,4 @@
+string = input("Enter the main string: ")
+substring = input("Enter the substring to remove: ")
+result = string.replace(substring, "")
+print("String after removing substring:", result)
